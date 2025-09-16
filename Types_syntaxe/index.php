dbbab5e1e59a9f6e9bfe5603d10973229ec62350
Essachi <essamrachiali@gmail.com>
2025-09-16 12:11:06 +0100

    first commit

---
+++ Types_syntaxe/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_type= 1);
+
+$input = [
+  'title'     => 'PHP 8 en pratique',
+  'excerpt'   => '',
+  'views'     => '300',
+  // 'published' absent
+  'author'    => 'Yassine'
+];
+
+
+
+function buildArticle(array $row){
+
+}
